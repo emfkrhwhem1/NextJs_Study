dc668c9bdfbc26bea315b52313b152c04781033c
emfkrhwhem1 <emfkrhwhem1@naver.com>
2026-06-11 19:53:23 +0900

    New Post , DataBases에 넣는 함수 추가 후 작업 완료

--- NextJs_Mutating_Data/app/new-post/page.js
+++ NextJs_Mutating_Data/app/new-post/page.js
@@ -1,12 +1,20 @@
-"use server";
+import { storePost } from "@/lib/posts";
 
 export default function NewPostPage() {
 
-  function createPost(formData) {
+  async function createPost(formData) {
+    "use server"
+
     const title = formData.get('title');
     const image = formData.get('image');
     const content = formData.get('content');
 
+    storePost({
+      imageUrl: '',
+      title,
+      content,
+      userId: 1
+    })
     console.log(title, image, content);
   }
 
