5190b9097b86702ab9e711640b61f2d84fd0b646
emfkrhwhem1 <emfkrhwhem1@naver.com>
2026-06-14 21:38:14 +0900

    MetaData 동적, 정적 공부

--- Optimizations/app/feed/page.js
+++ Optimizations/app/feed/page.js
@@ -1,6 +1,21 @@
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+
+/* export const metadata = {
+  title: 'All Posts',
+  description: 'Browser all our posts!'
+} */
+
+export async function generateMetadata() {
+  const posts = await getPosts();
+  const numberOfPosts = posts.length;
+  return {
+    title: `Browse all our ${numberOfPosts} posts.`,
+    description: 'Browse all our posts.'
+  }
+}
+
 export default async function FeedPage() {
   const posts = await getPosts();
   return (
